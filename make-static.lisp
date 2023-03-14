753ed19dc3527575c09a920def74466614279a79
Lily Appely <lappee@yahoo.com>
2023-03-14 10:56:27 +0300

    Restructure project && Fix some alignment

--- make-static.lisp
+++ make-static.lisp
@@ -6,15 +6,23 @@
                               ("lappee@yahoo.com"                 . "Email!")))
 
 (defparameter *content* 
-  '(("About me:"     . "Just another web-backend developer. I like lisp, aspecially Common lisp,
-                        Kitties and have no fear about JS and other frontend stuff. This paragraph will 
-                        complete a bit later. Nyahoi~")
-    ("Intrested in:" . #("Functional programming" "Web programming" "Plants growing"  "Lisp" "Jazz"))
-    ("Tool-Stack:"   . #("Common Lisp" "SML/Other ML's" "JS/HTML/CSS" "C#" "Vim"))))
+  `(("About me:" .
+     "Just another software developer. I like functional languages,
+     kitties and have no fear about JS and other frontend stuff.
+     This paragraph will complete a bit later. Nyahoi~")
+    ("Intrested in:" . 
+     #("Functional programming" "Web programming" "Plants growing"  "Compiler-craft"))
+    ("Programming-Stack:" .
+     #("Python" "JS/HTML/CSS" "SML/OCaml/MLs" "C#" "Lisps"))
+    ("Mopclip" .
+     ,(spinneret:with-html-string
+       (:span "Human-friendly monadic parser-combinator library for StandardML.
+               Lightweight and simple DSL library for parsers. "
+       (:a :attrs (list :href "https://github.com/poipoiPIO/mopclip") "Meow~"))))))
 
 (defun html-list-tree? (item)
   (and (not (simple-string-p item)) 
-       (vectorp item)))
+    (vectorp item)))
 
 (defparameter *css* (cl-css:css 
               `((@font-face :font-family "gdk9" 
@@ -33,7 +41,7 @@
                       :margin "4% auto"
                       :padding "9px 5px 15px" 
                       :background-color "#ffffe0"
-                      :opacity "0.8"
+                      :opacity "0.9"
                       :border "0.75rem ridge rgba(211, 220, 50, .6)")
 
                 (header :display flex
@@ -41,6 +49,9 @@
                         :flex-direction row
                         :padding "0.5rem 1rem"
                         :font-family pf7)
+                (header>.info
+                        :width 100%
+                        :margin-right 2rem)
 
                 (header>.avatar>img :width 16rem
                                     :border "1rem ridge rgba(211, 220, 50, .6)")
@@ -72,16 +83,16 @@
                   (header>.avatar>img :width 16rem
                                       :border "1rem ridge rgba(211, 220, 50, .6)"))
 
-                ("@media only screen and (max-width: 640px)" 
+                ("@media only screen and (max-width: 670px)" 
                   (header>.spacer :width 40%)
 
                   (.cattoes :height 2rem
                             :width 14rem)
 
-                  (header>.avatar>img :width 12rem
+                  (header>.avatar>img :width 13rem
                                       :border "1rem ridge rgba(211, 220, 50, .6)"))
 
-                ("@media only screen and (max-width: 540px)" 
+                ("@media only screen and (max-width: 590px)" 
                   (.cattoes :height 2rem
                             :width 16.7rem
                             :margin "0.5rem auto")
@@ -90,10 +101,13 @@
                   (.paragraph>p :margin-left 0rem
                                 :text-indent 2em
                                 :margin-right 1rem)
-                  (header>.avatar>img :width 100%
+                  (header>.avatar :width 100%
+                                  :display "flex"
+                                  :flex-direction "row"
+                                  :justify-content "center")
+                  (header>.avatar>img :width 85%
                                       :align-self center
-                                      :border "1rem ridge rgba(211, 220, 50, .6)"
-                                      :margin-left 10%)
+                                      :border "1rem ridge rgba(211, 220, 50, .6)")
 
                   (header :display flex
                           :flex-direction column
@@ -101,29 +115,27 @@
                           :font-family pf7)
                   (.spacer :height 1rem)))))
 
-(defvar *html* (spinneret:with-html-string 
-  (:html 
+(defvar *html* (spinneret:with-html-string (:html 
    (:head 
      (:meta :attrs (list :charset "utf-8"))
      (:meta :attrs (list :name "viewport" :content "width=device-width, initial-scale=1"))
      (:link :attrs (list :type "text/css" :rel "stylesheet" :href "./main.css"))
-     (:title "lappee-site<3"))
+     (:title "lappee-site <3"))
  ;; ---------Head-part-ends-----------
 
  ;; ---------Header-part--------------
    (:body 
     (:header 
-      (:div.info.width-100
+      (:div.info
         (:h1 "Lappely <3~")
           (:b (:em "Nyahoi~ y.o | He/Him | Rus") (:br)
-              "☃ An another one cute software developer~") (:br) (:br)
+              "☃ Another one cute software developer~") (:br) (:br)
           (loop for item in *my-contacts* do 
             (:a :href (if (str:contains? "email" (cdr item) :ignore-case t) 
                         (format nil "mailto: ~A" (car item)) 
                         (car item)) (cdr item)))
           (:div.cattoes))
-      (:div.spacer.width-100)
-      (:div.avatar.width-75 
+      (:div.avatar 
         (:img.avatar-image :attrs (list :src "https://github.com/poipoiPIO.png" :alt "Profile image"))))
 
   ;; --------Section-part----------------
@@ -132,15 +144,17 @@
         (:h4 (:span.sharp "#") 
              (car item)) ; car is the head of paragraph 
         (if (html-list-tree? (cdr item))
-          (:ul (loop for paragraph across (cdr item) do (:li paragraph)))
-          (:p (cdr item))))))
+          (:ul (loop 
+            for paragraph across (cdr item)
+              do (:li paragraph)))
+          (:p (:raw (cdr item)))))))
 
   ;; ---------Footer-part---------------
     (:footer 
-      (:sub "Made with secret alien technology " 
+      (:sub "Site made with secret alien technology " 
         (:a :attrs (list :href "https://www.github.com/poipoiPIO/my-little-site") "Meow~")))))))
 
 (defun make-markup ()
   (progn 
-    (str:to-file "./static/index.html" *html*)
-    (str:to-file "./static/main.css" *css*)))
+    (str:to-file "./index.html" *html*)
+    (str:to-file "./main.css" *css*)))
